6fb0cf62c32ace1bef62bfd6878288d71eaebe15
niklaas <niklaascharles@gmail.com>
2017-08-08 18:58:07 +0100

    Added Tinkering Space Sign

--- positionShim.js
+++ positionShim.js
@@ -5,8 +5,8 @@ window.addEventListener("resize", resize, false);
 window.addEventListener("orientationchange", resize, false);
 
 function resize() {
-  var container = 720 * (window.innerWidth/1280) - 10 + "px";
+  var container = 720 * (window.innerWidth/1280) - (window.innerHeight/8) + "px";
   var frame = window.innerHeight - 60 + "px";
-  
+
   wrap.style.top = frame < container ? frame : container;
 }

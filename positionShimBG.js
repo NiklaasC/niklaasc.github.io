dcd8da0390be174061b8fb4944955f1270d61e8f
niklaas <niklaascharles@gmail.com>
2017-09-17 13:39:44 +0100

    Fixed RockCam page and added separate positionShim for non-video backgrounds

--- positionShimBG.js
+++ positionShimBG.js
@@ -5,7 +5,8 @@ window.addEventListener("resize", resize, false);
 window.addEventListener("orientationchange", resize, false);
 
 function resize() {
-  var container = 720 * (window.innerWidth/1280) - (window.innerHeight/12) + "px";
+
+  var container = 1024 - (window.innerHeight/8) + "px";
   var frame = window.innerHeight - 60 + "px";
 
   wrap.style.top = frame < container ? frame : container;
